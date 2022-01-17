64a0b82b26d6082799811eedc82617fc5258e9f3
Dima <55377558+BogDoti228@users.noreply.github.com>
2022-01-17 08:18:39 +0500

    for-iphone-x

--- src/components/Plate.js
+++ src/components/Plate.js
@@ -2,8 +2,16 @@ import Card from "./Card";
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {back_gifs, fetch_gifs_api, filter_gifs} from "../store/fetch_slice";
+import {useMediaPredicate} from "react-media-hook";
 
 function Plate(){
+    let height = 400;
+    let width = 400;
+    const isIphoneX = useMediaPredicate("(width : 375px)")
+    if (isIphoneX) {
+        height = 355;
+        width = 355;
+    }
     const { gifs_objs } = useSelector((state) => state.fetch_reducer)
     const { objects, filtered } = useSelector((state) => state.controller_reducer)
     const dispatch = useDispatch()
@@ -24,7 +32,7 @@ function Plate(){
     return (
         <div className={"plate"}>
             {gifs_objs.map((obj) => {
-                return <Card gif={obj.gif} height={400} width={400} index={obj.index} filter={obj.filter}/>
+                return <Card gif={obj.gif} height={height} width={width} index={obj.index} filter={obj.filter}/>
             })}
         </div>
     );
